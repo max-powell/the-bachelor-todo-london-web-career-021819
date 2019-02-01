0a78c9c205ef1d7fc85ed82e331ad6dffe91ffcc
Max Powell <max_f_powell@hotmail.co.uk>
2019-02-01 14:31:14 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -42,7 +42,7 @@ end
 
 def get_average_age_for_season(data, season)
   ages = data[season].collect do |contestant|
-    contestant["age"].to_i
+    contestant["age"].to_f
   end
   ages.reduce(:+)/ages.length
 end
