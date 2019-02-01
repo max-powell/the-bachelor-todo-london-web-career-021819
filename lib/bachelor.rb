57a7c7bc7bbf12b5f8bcdeff74e25f1ac47083c9
Max Powell <max_f_powell@hotmail.co.uk>
2019-02-01 14:09:11 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,7 +3,7 @@ require "pry"
 def get_first_name_of_season_winner(data, season)
   data[season].each do |contestant|
     if contestant["status"] = "Winner"
-      return contestant["name"]
+      return contestant["name"].split[0]
     end
   end
 end
